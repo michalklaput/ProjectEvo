fc6260970eb5130621d66f2d2413464534e7b6bb
Your Name <you@example.com>
2018-05-24 19:17:54 +0200

    Dodaj pliki projektów.

---
+++ ProjectEvo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectEvo.Startup))]
+namespace ProjectEvo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
